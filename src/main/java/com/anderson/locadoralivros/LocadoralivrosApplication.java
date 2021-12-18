ac73d2caf404a5261a634df5231c7afa43259513
AndersonHelder93 <andersonhelder18@gmail.com>
2021-12-18 16:10:07 -0300

    commit inicial do projeto

---
+++ src/main/java/com/anderson/locadoralivros/LocadoralivrosApplication.java
@@ -0,0 +1,13 @@
+package com.anderson.locadoralivros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocadoralivrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocadoralivrosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anderson/locadoralivros/LocadoralivrosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anderson.locadoralivros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocadoralivrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
