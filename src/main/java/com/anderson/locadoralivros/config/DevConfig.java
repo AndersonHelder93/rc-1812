4c0e175da39c447cc69af2e85401713aa10b26f6
AndersonHelder93 <andersonhelder18@gmail.com>
2021-12-20 00:38:25 -0300

    criacao do perfil de dev

--- src/main/java/com/anderson/locadoralivros/config/DevConfig.java
+++ src/main/java/com/anderson/locadoralivros/config/DevConfig.java
@@ -1,6 +1,8 @@
 package com.anderson.locadoralivros.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
@@ -12,4 +14,15 @@ public class DevConfig {
 
 	@Autowired
 	private DBService dService;
+	
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String strategy;
+	
+	@Bean
+	public boolean instanciaBaseDeDaados() {
+		if(strategy.equals("create")) {
+			this.dService.instanciaBaseDeDados();
+		}
+		return false;
+	}
 }
